5333b6a705f8d4d166f1dedadb53f0cb97da3555
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2023-08-31 21:59:21 +0530

    add binarybits

---
+++ 06. BinaryBits/dataTypeDetails.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+
+public class dataTypeDetails{
+    public static void main(String arg[]){
+
+    int x=5;
+    System.out.println(Integer.toBinaryString(x));
+    //    output= 101
+
+    int y=-5;
+    System.out.println(Integer.toBinaryString(y));
+    //  output= 11111111111111111111111111111011
+    }
+}
