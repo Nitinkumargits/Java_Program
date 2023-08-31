071b77af19d3d5abbad668fa4454b039b9a2416d
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2023-08-31 19:54:38 +0530

    learn literals

---
+++ 05. literals/literals.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+
+public class variable{
+    public static void main(String arg[]){
+
+       byte a=127;
+       short s=140;
+       int  i=99;
+
+       byte b1=10;
+       byte b2=0b1010;
+       byte b3=012;
+       byte b4=0xA;
+
+
+       long l=9999999L;
+       float f=12.43f;
+       double d=12.43d;
+       
+
+
+    }
+}
