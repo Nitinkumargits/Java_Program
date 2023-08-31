6b2b82f5ab2677afed4d4c377ab5492e48bc6a14
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2023-08-31 17:39:50 +0530

    Add displayName java file

---
+++ 02. Reading_from_keyboard/displayName.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+
+public class displayName{
+    public static void main(String arg[]){
+        Scanner sc=new Scanner(System.in);
+
+        String name;
+        System.out.println("Enter the Name :-");
+        name=sc.nextLine();
+
+        System.out.println("Your Name is : "+ name);
+    }
+}
