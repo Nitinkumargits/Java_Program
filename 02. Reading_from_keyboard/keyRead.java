fb51bc2dc54ef34cff653f8e928cdcc5fe66a906
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2023-08-31 17:26:07 +0530

    Reading from keyboard

---
+++ 02. Reading_from_keyboard/keyRead.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+import java.util.*;
+
+
+public class keyRead{
+
+    public static void main(String arg[])  {
+     
+     Scanner sc=new Scanner(System.in);
+     int a,b,c;
+     System.out.println("Enter two value of A and B :");
+     a=sc.nextInt();
+     b=sc.nextInt();
+     
+     c=a+b;
+     System.out.println("Sum is :"+c);
+     
+
+    }
+}
