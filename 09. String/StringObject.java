66c6cad6ee55dd7e53f46928ae74789450a327d9
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2023-09-09 21:45:28 +0530

    learn about Stringobject

---
+++ 09. String/StringObject.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+
+class StringObject{
+    public  static void main(String arg[]){
+
+        String str="java program";
+        System.out.println(str);
+        System.out.println(str);
+        
+        char c[]={'A','B','C','D'};
+        String str1=new String(c);
+        System.out.println("character :" +str1);
+        
+        byte b[]={65,66,67,68};
+        String str2=new String(b);
+        System.out.println("byte :" +str2);
+
+        char k[]={'h','e','l','l','o'};
+        String str4=new String(k,1,3);
+        System.out.println(str4);//ell
+
+    }
+}
