6501dae9d3210bc092842685c9d08f161b12fc48
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2023-09-09 18:21:15 +0530

    learn more about flag and widht

--- 09. String/flagAndWidth.java
+++ 09. String/flagAndWidth.java
@@ -2,6 +2,21 @@ import java.lang.*;
 
 class flagAndWidth{
     public static void main(String arg[]){
-        
+        int a=10;
+        int b=-10;
+        System.out.printf("%5d\n",a);
+        System.out.printf("%05d\n",a);
+        System.out.printf("%(5d\n",b);
+        System.out.printf("%+5d\n",a);
+        System.out.printf("%+5d\n",b);
+
+        float f=12.3456f;
+        System.out.printf("%6.2f\n",f);
+        float k=8918122.45f;
+        System.out.printf("%6.2f\n",k);
+
+        String  str="java";
+        System.out.printf("%20S\n",str);
+        System.out.printf("%-20S",str);
     }
 }
