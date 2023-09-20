b217e4bbe39d4b5d93b76c8b689b1855f22ee47f
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2023-09-20 22:55:37 +0530

    loop

---
+++ 11. loop/loopy.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+class loopy {
+    public static void main(String arg[]) {
+        Scanner sc = new Scanner(System.in);
+
+        // Example 1
+
+        // Display multiplication table
+        // 5* i=5*i
+        // System.out.println("Enter the value of table: ");
+
+        // int n = sc.nextInt();
+        // for (int i = 1; i <= 10; i++) {
+        // System.out.println(n + " * " + i + " = " + (n * i));
+        // }
+
+        // Example 2
+
+        // Sum of n number
+        // System.out.printf("Enter the value : ");
+
+        // int n = c
+        // int sum = 0;
+        // for (int i = 1; i <= n; i++) {
+        // sum += i;
+        // }
+        // System.out.println(sum);
+
+        // // Example 3
+
+        // // Display the digit of number .
+        // System.out.printf("Enter the value : ");
+        // int n = sc.nextInt();
+        // int r;
+        // while (n > 0) {
+
+        // r = n % 10;
+        // n = n / 10;
+        // System.out.println(r);
+        // }
+
+        // Example 4
+
+        // Nested loop
+
+        // for (int row = 1; row <= 8; row++) {
+
+        // for (int col = 1; col <= 8; col++) {
+
+        // System.out.printf(" (" + row + "," + col + ")");
+        // }
+        // System.out.println();
+        // }
+
+        // Example 5
+    }
+}
