4cea46e9e7ca934b6b0e07cc14f3d675a408c3f5
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2023-09-26 18:55:01 +0530

    Reverse An arary

---
+++ 12. Array/ReverseCopy.java
@@ -0,0 +1,20 @@
+public class ReverseCopy {
+    public static void main(String[] args) {
+        int A[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int len = A.length;
+        int B[] = new int[len];
+
+        for (int el : A) {
+            System.out.printf(el + " ");
+        }
+        System.out.println("");
+
+        for (int i = 0, j = A.length - 1; i < A.length; i++, j--) {
+            B[j] = A[i];
+        }
+
+        for (int el : B) {
+            System.out.printf(el + " ");
+        }
+    }
+}
