d4d6790d1aa83fe1d49ab9e7b37572e8d650b4de
Nitin kumar <104756638+Nitinkumargits@users.noreply.github.com>
2023-10-03 20:42:12 +0530

    method intro

---
+++ 13. Method/intro.java
@@ -0,0 +1,19 @@
+public class intro {
+
+    static int max(int x, int y) {
+        if (x > y) {
+            return x;
+        } else {
+            return y;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int a = 10, b = 20, c;
+
+        c = max(a, b);
+        System.out.println(c);
+
+    }
+}
